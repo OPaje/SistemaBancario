f941e39f0355e28064bbddb9799fdfafcefa3bee
OPaje <jecssilva@gmail.com>
2023-05-29 11:06:27 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaBancario/Conta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario
+{
+    class Conta
+    {
+        public int Agencia { get; set; }
+        public int NumeroConta { get; set; }
+        public String NomeConta { get; set;}
+        public Titular Titular { get; set; }
+        public Decimal Saldo { get; set; }
+
+        public Decimal verSaldo()
+        {
+            return Saldo;
+        }
+
+        public void depositar(decimal valor)
+        {
+            Saldo += valor;
+        }
+
+        public void sacar(decimal valor)
+        {
+            if(valor < Saldo){
+                Saldo -= valor;
+            }
+            else{
+                Console.WriteLine("Não foi possível fazer o saque. Saldo insuficiente");
+            }
+            
+        }
+    }
+
+}
---
+++ SistemaBancario/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = new Conta();
+            conta.NomeConta = "Conta Conta";
+            conta.NumeroConta = 1010;
+            conta.Saldo = 1000;
+            conta.Agencia = 001;
+
+            conta.Titular = new Titular()
+            {
+                Nome = "Jean",
+                Cpf = "1232434",
+                Rg = "MG-098322",
+                Endereco = "Brasil"
+
+            };
+
+            Console.WriteLine($"Saldo atual: {conta.verSaldo()}");
+
+            conta.depositar(500);
+
+            Console.WriteLine($"Saldo depois do deposito: {conta.verSaldo()}");
+
+            conta.sacar(500);
+
+            Console.WriteLine($"Saldo depois do saque: {conta.verSaldo()}");
+
+            conta.sacar(1550);
+
+            Console.ReadKey();
+        }
+    }
+}
